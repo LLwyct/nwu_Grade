284661adb62835e3fc3f0869b67fd556a20688a1
outsider <787552171@qq.com>
2018-01-09 12:28:03 +0800

    整理代码风格

--- Main_Window.py
+++ Main_Window.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import images_qr
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QIcon, QFont
@@ -15,10 +16,13 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        # 顶部的水平布局labelEdit+button
+
+        # 设置全局字体大小
+        self.setFont(QFont('微软雅黑', 10))
+
+        # 顶部的水平布局label+labelEdit+button
         self.top = QHBoxLayout()
         label = QLabel('学号')
-        self.setFont(QFont('微软雅黑', 10))
         self.input = QLineEdit()
         query = QPushButton('查询')
         query.clicked.connect(self.Query)
@@ -26,63 +30,82 @@ class Example(QWidget):
         self.top.addWidget(self.input)
         self.top.addWidget(query)
 
-        # 水平布局选择查询类型
+        # 位于中间的布局选择查询类型
         typeselect = QHBoxLayout()
         self.rbtn_1 = QRadioButton('本学期成绩(推荐)')
         rbtn_2 = QRadioButton('历史全部成绩')
+        # 默认选中
         self.rbtn_1.toggle()
         self.rbtn_1.toggled.connect(self.rbtnchange)
         typeselect.addWidget(self.rbtn_1)
         typeselect.addWidget(rbtn_2)
 
+        # 底部的表格
+        self.table = QTableWidget()
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
         # 全局垂直布局
         self.body = QVBoxLayout()
         self.body.addLayout(self.top)
         self.body.addLayout(typeselect)
-        self.table = QTableWidget()
-        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.body.addWidget(self.table)
+
         #设置窗口的位置和宽高
         self.setLayout(self.body)
         self.center()
         self.resize(800, 600)
+
         #设置窗口标题
-        self.setWindowTitle('成绩查询(请使用校园网哦~)')
-        self.setWindowIcon(QIcon('dog.jpg'))
+        self.setWindowTitle('GradeViewer')
+        self.setWindowIcon(QIcon(':/dog.jpg'))
         self.show()
         QMessageBox.information(self, '提示', '请使用校园网查询(￣▽￣)~*')
 
     def Query(self):
         length = -1
+        if self.TYPE == 2:
+            reply = QMessageBox.question(self, '重要通知(｀Д´)', '历史查询请耐心等待20s以上!!!   ', QMessageBox.Yes | QMessageBox.No,
+                                         QMessageBox.Yes)
+            if reply == QMessageBox.No:
+                reply = QMessageBox.information(self, '不守规则是吧', '我也是有脾气的好吧￣へ￣')
+                QCoreApplication.instance().quit()
         result = Query(self.input.text(), type=self.TYPE)
         if result == 'ERROR':
-            QMessageBox.warning(self, '警告', '出现了未知的错误，请检查输入')
+            QMessageBox.warning(self, '警告', '出现了未知的错误，请检查输入或是否使用校园网')
             return
+        # 判断数据长度，若大于0
         if len(result[0]) > 0:
             length =  len(result[0])
-        self.table.setRowCount(length)
+        elif len(result) == 0:
+            QMessageBox.warning(self, '警告', '未查询到成绩哦~')
+            return
 
-        if self.TYPE == 1:
-            self.table.setColumnCount(2)
-            for i in range(2):
-                for j in range(length):
-                    self.table.setItem(j, i, QTableWidgetItem(result[i][j]))
-        else:
-            self.table.setColumnCount(3)
-            for i in range(3):
-                for j in range(length):
-                    self.table.setItem(j, i, QTableWidgetItem(result[i][j]))
+        if length > 0:
+            self.table.setRowCount(length)
+            # 如果有数据，则根据类型决定列数
+            if self.TYPE == 1:
+                self.table.setColumnCount(2)
+                self.table.setHorizontalHeaderLabels(['课程', '成绩'])
+                self.table.setColumnWidth(0, 300)
+                self.table.setColumnWidth(1, 200)
+                for i in range(2):
+                    for j in range(length):
+                        self.table.setItem(j, i, QTableWidgetItem(result[i][j]))
+            elif self.TYPE == 2:
+                self.table.setColumnCount(3)
+                self.table.setHorizontalHeaderLabels(['课程', '成绩', '学期'])
+                self.table.setColumnWidth(0, 300)
+                self.table.setColumnWidth(1, 200)
+                self.table.setColumnWidth(2, 150)
+                for i in range(3):
+                    for j in range(length):
+                        self.table.setItem(j, i, QTableWidgetItem(result[i][j]))
 
-        # self.body.addWidget(table)
 
     def rbtnchange(self):
         if self.rbtn_1.isChecked():
             self.TYPE = 1
         else:
-            reply = QMessageBox.question(self, '重要通知(｀Д´)', '历史查询请耐心等待20s以上!!!   ', QMessageBox.Yes|QMessageBox.No, QMessageBox.No)
-            if reply == QMessageBox.No:
-                reply = QMessageBox.information(self, '不守规则是吧','我也是有脾气的好吧￣へ￣')
-                QCoreApplication.instance().quit()
             self.TYPE = 2
 
     def center(self):
--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 # coding: utf-8
 import requests
 from bs4 import BeautifulSoup
-from xml_list import xmls
 from PrettyPrint import prettyPrint
 
-input_username = '2015118130'
+
+xmls = {
+
+    'preDo': '''<?xml version="1.0" encoding="utf-8"?>
+                <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                  <soap:Body>
+                    <loginverifystudent_B xmlns="http://murpcn.com/murpwebservice/">
+                      <loginname>2015118140</loginname>
+                      <loginpass>2015118140</loginpass>
+                      <tec>string</tec>
+                      <logininfo>string</logininfo>
+                      <strbak>string</strbak>
+                    </loginverifystudent_B>
+                  </soap:Body>
+                </soap:Envelope>
+                ''',
+
+    'getAllGrade': '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><GetMyGrades xmlns="http://murpcn.com/murpwebservice/"><umcid>80213</umcid></GetMyGrades></soap:Body></soap:Envelope>',
+
+    'getTermGrage': '''<?xml version="1.0" encoding="utf-8"?>
+                        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                          <soap:Body>
+                            <Mark xmlns="http://murpcn.com/murpwebservice/">
+                              <umcid>80213</umcid>
+                            </Mark>
+                          </soap:Body>
+                        </soap:Envelope>
+                        ''',
+}
 
 headers = {
 
@@ -31,36 +58,46 @@ headers = {
 }
 
 urls = {
+
     'preDo': 'http://219.245.18.61:81/MURPWebService/MURPMainService.asmx',
+
     'getAllGrade' : 'http://219.245.18.61:81/KEY/MURPNewsService.asmx',
+
     'getTermGrage' : 'http://219.245.18.61:81/KEY/MURPNewsService.asmx',
 }
 
 
 def Query(input_username, type):
+
     xml = xmls['preDo'].replace('2015118140', input_username)
     # 向接口发出带有xml的post请求
-    response = requests.post(urls['preDo'], headers=headers['preDo'], data=xml)
-    # 用bs解析响应文本
-    soup = BeautifulSoup(response.text, 'lxml')
+    try:
+        response = requests.post(urls['preDo'], headers=headers['preDo'], data=xml, timeout=3)
+        # 用bs解析响应文本
+        soup = BeautifulSoup(response.text, 'lxml')
+    except:
+        return 'ERROR'
     # 得到关键信息
-
     try:
         _key = soup.umcid.string
     except:
         return 'ERROR'
+
     # 第二步再进行查询
     querystring = ''
     if type == 2:
         querystring = 'getAllGrade'
     elif type == 1:
         querystring = 'getTermGrage'
-
+    if querystring == '':
+        return 'ERROR'
     xml = xmls[querystring].replace('80213', _key)
-    response = requests.post(urls[querystring], headers=headers[querystring], data=xml)
-
-    querylist = prettyPrint(BeautifulSoup(response.text, 'lxml'), type=querystring)
-
+    try:
+        response = requests.post(urls[querystring], headers=headers[querystring], data=xml, timeout=60)
+        querylist = prettyPrint(BeautifulSoup(response.text, 'lxml'), type=querystring)
+    except:
+        return 'ERROR'
+    # 返回最终结果
     return querylist
 
 
